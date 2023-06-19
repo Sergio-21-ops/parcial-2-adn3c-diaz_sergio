92390b2bc9f28ed14fe36512c40533ef502053e0
Sergio-21-ops <sergio.diaz@davinci.edu.ar>
2023-06-19 17:29:33 -0300

    v-footer con nombre

--- app_prenpub/src/App.vue
+++ app_prenpub/src/App.vue
@@ -3,6 +3,13 @@
     <v-main>
       <router-view/>
     </v-main>
+    <v-footer class="v-footer black">
+    <v-row justify="center" no-gutters>
+      <v-col class="white--text text-center mt-4" cols="12">
+        <p><strong>Sergio Diaz 2 Parcial 3ADMTN</strong></p>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
