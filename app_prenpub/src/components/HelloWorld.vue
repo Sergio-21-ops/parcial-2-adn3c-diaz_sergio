8323cbd91802a64a8f9d406e652d89df7cc9d7bb
Sergio-21-ops <sergio.diaz@davinci.edu.ar>
2023-06-19 17:33:34 -0300

    cambio jerarquia h1 y 2

--- app_prenpub/src/components/HelloWorld.vue
+++ app_prenpub/src/components/HelloWorld.vue
@@ -11,11 +11,11 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h2 class="text-center my-5">Bienvenidos a Prenpub</h2>
+        <h1 class="text-center my-5">Bienvenidos a Prenpub</h1>
   <p class="text-left">Te damos la bienvenida a nuestra aplicacion dedicada exclusivamente para quienes buscan proyectar su propio espacio abocado al publico para difundir y compartir sus opiniones e intereses , dedicado especificamente a aquel quien siente que merece ser leido entre mas usuarios.</p>
   <p class="text-left">Prensapub nace en 2020 despues de la fusion entre la productora Prensa entre nos y publico para todos  con el objetivo de llegar a cada vez mas personas interesadas en este proyecto, si bien somos relativamente nuevos en el ambiente nuestras ideas en menos de 4 años logramos llegar a una audiencia bastante amplia en variedad de edad y centrada en la diversidad de categorias distinguidas entre entrevistas, editoriales, opiniones y columnas  .</p>	
   <p class="text-left">Nuestro staff esta compuesto por un grupo de personas jovenes, algunos estudiantes de periodismo, otros en las puertas de su recibimiento en la carrera ,otros con la carrera consolidada y al frente de una inmejorable oportunidad para crecer con el tiempo,  a todos estos los une la conjugacion de las dos palabras en comun que dieron origen a esta idea. Tenemos a su disposición Nuestros contenidos se rigen con una amplia variedad de temas y le es util a quien quiera hacer sus primeras armas en el mundo del periodismo o bien para otro publico el cual simplementa quiera dar a conocer su voz en este ambiente.</p>
-  <h3 class="text-center my-5">Por que descargarse nuestra aplicacion</h3>
+  <h2 class="text-center my-5">Por que descargarse nuestra aplicacion</h2>
         
 <v-list>
   <v-list-item
