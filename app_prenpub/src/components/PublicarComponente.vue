bb5eb8365944325e703f5a56f7ede5b1074bba2a
Sergio-21-ops <sergio.diaz@davinci.edu.ar>
2023-06-22 22:29:59 -0300

    data y componente publicar

--- app_prenpub/src/components/PublicarComponente.vue
+++ app_prenpub/src/components/PublicarComponente.vue
@@ -20,30 +20,18 @@
   
   <script>
     export default {
-      name: 'HelloWorld',
+      name: 'PublicarComponente',
   
-      data: () => ({
-        tips: [
-          {
-                      title: "Alcance", 
-                      img: require('../assets/alcance.jpg'), 
-                      alt:"Imagen de alcance",
-                      texto:"Nuestra aplicacion permite relacionar su contenido a un largo nivel de difusion junto con el de otros usuarios, lo cual permite subir su calificacion y facilitar su reputacion ",
-                  },
-                  {
-            title: "Experiencia", 
-                      img: require('../assets/experiencia.jpg'), 
-                      alt:"Imagen de experiencia",
-                      texto:"No importa su nivel de conocimiento en la aplicacion o en el ambiente periodistico, la app le permite ir evolucionando su contenido en base a sugerencias a nivel de lectura u ortografico",
-                  },
-                  {
-            title: "Desarrollo", 
-                      img: require('../assets/desarrollo.jpg'), 
-                      alt:"Imagen desarrollo",
-                      texto:"Pese a estar poco tiempo en play store, la app tiende a mejorar cada cierto tiempo para facilitar la fluidez de la usabilidad",
-                  }
-              ],
-      }),
-    }
+      data: function() {
+        return {
+			form_data:{
+				titulo:"",
+				autor:"",
+				cuerpo:"",
+				sinopsis:"",
+			},
+		verificar:true,
+		arr:[]
+    }}}
   </script>
   
